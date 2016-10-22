676dbbdadcc598aed4753d49295c43954722556f
efcuser <efcuser@efclassroom.idmt.fraunhofer.de>
2016-10-22 10:59:13 +0200

    Addes Lava

---
+++ Assets/Scripts/Interaction/Beam.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beam : MonoBehaviour 
+{
+	#region Public Vars
+
+	public GameObject m_ObjectToBeam;
+
+	#endregion
+
+	#region Private Vars
+
+	GameVRController m_ControllerEvents;
+	LaserPointer m_LaserPointer;
+
+	#endregion
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_ControllerEvents = GetComponent<GameVRController>();
+		m_LaserPointer     = GetComponent<LaserPointer>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (m_ControllerEvents.GetPressUpTouchpadButton1()) 
+		{
+			BeamObject();
+		}
+	}
+
+	private void BeamObject()
+	{
+		m_ObjectToBeam.transform.position = m_LaserPointer.m_HitPoint;
+	}
+}
---
+++ Assets/Scripts/Interaction/InteractableObject.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableObject : MonoBehaviour
+{
+
+    #region Public Vars
+
+    public float m_VelocityFactor = 20000.0f;
+    public float m_RotationFactor = 400.0f;
+
+    #endregion
+
+    #region Private Vars
+
+    private bool m_IsGrabbed;
+
+    private Rigidbody m_Rigidbody;
+
+    private Grabber m_CurrentHandGrabbing;
+
+    private Transform m_PointOfInteraction;
+
+    #endregion
+
+    // -----------------------------------------------------------------------------
+
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        m_PointOfInteraction = new GameObject().transform;
+
+        m_VelocityFactor /= m_Rigidbody.mass;
+        m_RotationFactor /= m_Rigidbody.mass;
+    }
+
+    // -----------------------------------------------------------------------------
+
+    void Update()
+    {
+        if (m_CurrentHandGrabbing && m_IsGrabbed)
+        {
+            Vector3 VectorBetweenHandAndInteraction = m_CurrentHandGrabbing.transform.position - m_PointOfInteraction.position;
+            this.m_Rigidbody.velocity = VectorBetweenHandAndInteraction * m_VelocityFactor * Time.fixedDeltaTime;
+
+            Quaternion RotationOfHand = m_CurrentHandGrabbing.transform.rotation * Quaternion.Inverse(m_PointOfInteraction.rotation);
+
+            float AngleInDegree;
+            Vector3 RotationAxis;
+
+            RotationOfHand.ToAngleAxis(out AngleInDegree, out RotationAxis);
+
+            if (AngleInDegree > 180)
+            {
+                AngleInDegree -= 360;
+            }
+
+			this.m_Rigidbody.angularVelocity = (Time.deltaTime * AngleInDegree * RotationAxis) * m_RotationFactor;
+        }
+    }
+
+    // -----------------------------------------------------------------------------
+
+    public void BeginInteraction(Grabber _Hand)
+    {
+        m_CurrentHandGrabbing = _Hand;
+
+        m_PointOfInteraction.position = _Hand.transform.position;
+        m_PointOfInteraction.rotation = _Hand.transform.rotation;
+        m_PointOfInteraction.SetParent(transform, true);
+
+        m_IsGrabbed = true;
+    }
+
+    // -----------------------------------------------------------------------------
+
+    public void EndInteraction(Grabber _Hand)
+    {
+        if (m_CurrentHandGrabbing == _Hand)
+        {
+            m_CurrentHandGrabbing = null;
+            m_IsGrabbed           = false;
+        }
+    }
+
+    // -----------------------------------------------------------------------------
+
+    public bool IsInteracting()
+    {
+        return m_IsGrabbed;
+    }
+}
