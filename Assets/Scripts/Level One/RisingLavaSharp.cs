9a5d4292fc99429de5dc9e095c5d7fb326873c27
Unknown <t.schwandt@gmail.com>
2016-10-23 09:13:11 +0200

    Fixed winning condition bug

--- Assets/Scripts/Level One/RisingLavaSharp.cs
+++ Assets/Scripts/Level One/RisingLavaSharp.cs
@@ -10,6 +10,8 @@ public class RisingLavaSharp : MonoBehaviour {
 	public float currentTime = 0.0f;
 	public WinArea m_WinAreaScript;
 
+	private bool m_IsActive = true;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -17,13 +19,20 @@ public class RisingLavaSharp : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (!m_IsActive)
+			return;
+
 		currentTime = currentTime + Time.deltaTime;
 		if (transform.position.y < maxHeight) {
 			transform.Translate(Vector3.up * Time.deltaTime * Speed, Space.World);
 		} else {
 			if (currentTime >= 8) {
-				if(m_WinAreaScript.GetIsInside()){
+				if(m_WinAreaScript.GetIsInside())
+				{
                     player.transform.position = new Vector3(m_TargetPosition.position.x, m_TargetPosition.position.y, m_TargetPosition.position.z);
+
+					m_IsActive = false;
 				}else{
 					Application.LoadLevel("Death");
 				}
