7d8c4083819375966b07bfe7c327224faeaeeef7
Unknown <t.schwandt@gmail.com>
2016-10-23 10:46:39 +0200

    Implemented onHitStart for StartScene Intro

---
+++ Assets/Scripts/Introduction/onHitStartArea.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+var yTrigger = 33.0;
+
+function Update () {
+	if (transform.position.y < yTrigger) {
+		Debug.Log("You are dead. Respawn imminent...");
+		Application.LoadLevel("StartScene");
+	}
+}
